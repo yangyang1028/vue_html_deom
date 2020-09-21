59e0161662dabf9ac5bb8f6902a05b5f5547b002
admin <1799927192@qq.com>
2020-09-21 16:33:05 +0800

    project init

--- src/main.js
+++ src/main.js
@@ -3,8 +3,15 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import fastClick from 'fastclick'
+// 解决link延迟问题
+import './assets/styles/reset.css'
+// 样式保持同意
+import './assets/styles/border.css'
+// 解决移动端1像素边框的问题
 
 Vue.config.productionTip = false
+fastClick.attach(document.body)
 
 /* eslint-disable no-new */
 new Vue({
--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,10 +1,12 @@
 <template>
-  <div id="app"><router-view/></div>
+<div>
+  <div>Hello world</div>
+</div>
 </template>
 
 <script>
 export default {
-  name: 'App'
+  name: 'Home'
 }
 </script>
 
