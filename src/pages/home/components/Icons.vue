d2359a3a464dd7adf4673e1b8a43fa1238d13f45
admin <1799927192@qq.com>
2020-09-23 16:33:34 +0800

    add icons

---
+++ src/pages/home/components/Icons.vue
@@ -0,0 +1,114 @@
+<template>
+  <div class="icons">
+    <swiper>
+      <swiper-slide v-for="(page, index) of pages" :key="index">
+        <div class="icon" v-for="item of page" :key="item.id">
+          <div class="icon-img">
+              <img class="icon-img-content" :src="item.imgURL" />
+          </div>
+          <p class="icon-text">{{item.imgText}}</p>
+        </div>
+      </swiper-slide>
+    </swiper>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Icons',
+  data () {
+    return {
+      iconList: [{
+        id: '001',
+        imgURL: require('@/assets/img/icon_01.png'),
+        imgText: '旅游景点'
+      }, {
+        id: '002',
+        imgURL: require('@/assets/img/icon_01.png'),
+        imgText: '旅游景点'
+      }, {
+        id: '003',
+        imgURL: require('@/assets/img/icon_01.png'),
+        imgText: '旅游景点'
+      }, {
+        id: '004',
+        imgURL: require('@/assets/img/icon_01.png'),
+        imgText: '旅游景点'
+      }, {
+        id: '005',
+        imgURL: require('@/assets/img/icon_01.png'),
+        imgText: '旅游景点'
+      }, {
+        id: '006',
+        imgURL: require('@/assets/img/icon_01.png'),
+        imgText: '旅游景点'
+      }, {
+        id: '007',
+        imgURL: require('@/assets/img/icon_01.png'),
+        imgText: '旅游景点'
+      }, {
+        id: '008',
+        imgURL: require('@/assets/img/icon_01.png'),
+        imgText: '旅游景点'
+      }, {
+        id: '009',
+        imgURL: require('@/assets/img/icon_01.png'),
+        imgText: '旅游景点'
+      }]
+    }
+  },
+  computed: {
+    pages () {
+      const pages = []
+      this.iconList.forEach((item, index) => {
+        const page = Math.floor(index / 8)
+        // 以下算法是将数组转换为二维数组
+        if (!pages[page]) {
+          pages[page] = []
+        }
+        pages[page].push(item)
+      })
+      return pages
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+@import "~styles/varibles.styl"
+@import "~styles/mixins.styl"
+  .icons
+    overflow: hidden
+    height: 0
+    padding-bottom: 50%
+    .icon
+      position: relative
+      float: left
+      overflow: hidden
+      width: 25%
+      height: 0
+      padding-bottom: 25%
+      .icon-img
+        position: absolute
+        top: 0
+        left: 0
+        right: 0
+        bottom: .44rem
+        box-sizing: border-box
+        padding: .1rem
+        width: 100%
+        .icon-img-content
+          display: block
+          margin: 0 auto
+          height: 100%
+      .icon-text
+        position: absolute
+        left: 0
+        right: 0
+        bottom: 0
+        padding-left: .1rem
+        line-height: .44rem
+        text-align: center
+        color: $textColr
+        ellipsis()
+</style>
