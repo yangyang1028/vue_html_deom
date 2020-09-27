92db701c5d97e04cb1486a8c64b1f37ad40a8896
admin <1799927192@qq.com>
2020-09-27 14:33:59 +0800

    add alphabet and list

--- src/pages/city/components/List.vue
+++ src/pages/city/components/List.vue
@@ -1,88 +1,94 @@
 <template>
-  <div class="border-list">
-    <div class="ares">
-      <div class="title border-topbottom">当前城市</div>
-      <div class="city-button">
-        <div class="button-wrapper">
-          <div class="button">北京</div>
+  <div class="border-list" ref="wrapper">
+    <div>
+      <div class="ares">
+        <div class="title border-topbottom">当前城市</div>
+        <div class="city-button">
+          <div class="button-wrapper">
+            <div class="button">北京</div>
+          </div>
         </div>
       </div>
-    </div>
-    <div class="ares">
-      <div class="title border-topbottom">热门城市</div>
-      <div class="city-button">
-        <div class="button-wrapper">
-          <div class="button">北京</div>
-        </div>
-        <div class="button-wrapper">
-          <div class="button">北京</div>
-        </div>
-        <div class="button-wrapper">
-          <div class="button">北京</div>
-        </div>
-        <div class="button-wrapper">
-          <div class="button">北京</div>
-        </div>
-        <div class="button-wrapper">
-          <div class="button">北京</div>
-        </div>
-        <div class="button-wrapper">
-          <div class="button">北京</div>
-        </div>
-        <div class="button-wrapper">
-          <div class="button">北京</div>
-        </div>
-        <div class="button-wrapper">
-          <div class="button">北京</div>
+      <div class="ares">
+        <div class="title border-topbottom">热门城市</div>
+        <div class="city-button">
+          <div class="button-wrapper">
+            <div class="button">北京</div>
+          </div>
+          <div class="button-wrapper">
+            <div class="button">北京</div>
+          </div>
+          <div class="button-wrapper">
+            <div class="button">北京</div>
+          </div>
+          <div class="button-wrapper">
+            <div class="button">北京</div>
+          </div>
+          <div class="button-wrapper">
+            <div class="button">北京</div>
+          </div>
+          <div class="button-wrapper">
+            <div class="button">北京</div>
+          </div>
+          <div class="button-wrapper">
+            <div class="button">北京</div>
+          </div>
+          <div class="button-wrapper">
+            <div class="button">北京</div>
+          </div>
         </div>
       </div>
-    </div>
-    <div class="ares">
-      <div class="title border-topbottom">A</div>
-      <div class="item-list">
-        <div class="list border-bottom">阿里尔</div>
-        <div class="list border-bottom">阿里尔</div>
-        <div class="list border-bottom">阿里尔</div>
-        <div class="list border-bottom">阿里尔</div>
-        <div class="list border-bottom">阿里尔</div>
+      <div class="ares">
+        <div class="title border-topbottom">A</div>
+        <div class="item-list">
+          <div class="list border-bottom">阿里尔</div>
+          <div class="list border-bottom">阿里尔</div>
+          <div class="list border-bottom">阿里尔</div>
+          <div class="list border-bottom">阿里尔</div>
+          <div class="list border-bottom">阿里尔</div>
+        </div>
       </div>
-    </div>
-    <div class="ares">
-      <div class="title border-topbottom">B</div>
-      <div class="item-list">
-        <div class="list border-bottom">阿里尔</div>
-        <div class="list border-bottom">阿里尔</div>
-        <div class="list border-bottom">阿里尔</div>
-        <div class="list border-bottom">阿里尔</div>
-        <div class="list border-bottom">阿里尔</div>
+      <div class="ares">
+        <div class="title border-topbottom">B</div>
+        <div class="item-list">
+          <div class="list border-bottom">阿里尔</div>
+          <div class="list border-bottom">阿里尔</div>
+          <div class="list border-bottom">阿里尔</div>
+          <div class="list border-bottom">阿里尔</div>
+          <div class="list border-bottom">阿里尔</div>
+        </div>
       </div>
-    </div>
-    <div class="ares">
-      <div class="title border-topbottom">C</div>
-      <div class="item-list">
-        <div class="list border-bottom">阿里尔</div>
-        <div class="list border-bottom">阿里尔</div>
-        <div class="list border-bottom">阿里尔</div>
-        <div class="list border-bottom">阿里尔</div>
-        <div class="list border-bottom">阿里尔</div>
+      <div class="ares">
+        <div class="title border-topbottom">C</div>
+        <div class="item-list">
+          <div class="list border-bottom">阿里尔</div>
+          <div class="list border-bottom">阿里尔</div>
+          <div class="list border-bottom">阿里尔</div>
+          <div class="list border-bottom">阿里尔</div>
+          <div class="list border-bottom">阿里尔</div>
+        </div>
       </div>
-    </div>
-    <div class="ares">
-      <div class="title border-topbottom">D</div>
-      <div class="item-list">
-        <div class="list border-bottom">阿里尔</div>
-        <div class="list border-bottom">阿里尔</div>
-        <div class="list border-bottom">阿里尔</div>
-        <div class="list border-bottom">阿里尔</div>
-        <div class="list border-bottom">阿里尔</div>
+      <div class="ares">
+        <div class="title border-topbottom">D</div>
+        <div class="item-list">
+          <div class="list border-bottom">阿里尔</div>
+          <div class="list border-bottom">阿里尔</div>
+          <div class="list border-bottom">阿里尔</div>
+          <div class="list border-bottom">阿里尔</div>
+          <div class="list border-bottom">阿里尔</div>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import Bscroll from 'better-scroll'
 export default {
-  name: 'CityList'
+  name: 'CityList',
+  mounted () {
+    this.scroll = new Bscroll(this.$refs.wrapper)
+  }
 }
 </script>
 
@@ -104,7 +110,7 @@ export default {
     right: 0
     bottom: 0
     .title
-      line-height: .44rem
+      line-height: .54rem
       background: #eeeeee
       font-size: .26rem
       padding-left: .2rem
