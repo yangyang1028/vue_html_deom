b2494d1049812117a97f314b3e92f75a50f8a38d
admin <1799927192@qq.com>
2020-10-16 14:32:07 +0800

    页面详情页-header

--- src/pages/detali/Detali.vue
+++ src/pages/detali/Detali.vue
@@ -1,19 +1,24 @@
 <template>
   <div>
     <detali-banner></detali-banner>
+    <detali-header></detali-header>
+    <div class="heightSize"></div>
   </div>
 </template>
 
 <script>
 import DetaliBanner from './components/banner'
+import DetaliHeader from './components/header'
 export default {
   name: 'Detali',
   components: {
-    DetaliBanner
+    DetaliBanner,
+    DetaliHeader
   }
 }
 </script>
 
 <style lang="stylus" scoped>
-
+  .heightSize
+    height: 50rem
 </style>
